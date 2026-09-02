d43e9dfa66abe47e59a69a96ee82c4c821f11a3a
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-02 22:35:31 +0800

    Create Day01.java
    Day01

---
+++ Day01.java
@@ -0,0 +1,16 @@
+package 100DaysofCoding;
+
+public class day001 {
+    
+    public static void main(String[] args) {
+        //single—line comment
+        String nama= "nurul";
+        //multi—line comment
+        int angka1=2;
+        int angka2=3;
+        int hasil=angka1 + angka2;
+        //menampilkan output ke layar
+        System.out.println("Halo"+ nama +"hasil penjumlahan: "+ hasil);
+    }
+    
+}
