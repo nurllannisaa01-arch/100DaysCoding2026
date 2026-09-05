103397412e6df17144c6135f76d8e844badab7bb
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-05 21:10:41 +0800

    Create day04.java
    berhasil

---
+++ day04.java
@@ -0,0 +1,14 @@
+public class day04 {
+    
+    public static void main(String[] args) {
+        byte umur=18;
+        short tahunLahir=2008;
+        int duitJajan=10000;
+        long jarakRumah=100000L;
+        System.out.println("Umur: "+umur);
+        System.out.println("Tahun lahir: "+tahunLahir);
+        System.out.println("Duit jajan: "+duitJajan);
+        System.out.println("Jarak Rumah: "+jarakRumah);
+    }
+    
+}
