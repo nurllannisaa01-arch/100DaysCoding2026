2360006e14c803a460f76cce238f3a32fedddd32
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-06 06:45:13 +0800

    Create day05.java
    ok

---
+++ day05.java
@@ -0,0 +1,10 @@
+public class day05 {
+    
+    public static void main(String[] args) {
+        double hargaJilbab=12000.50;
+        float ipk=3.17f;
+        System.out.println("Harga jilbab: "+hargaJilbab);
+        System.out.println("Ipk: "+ipk);
+    }
+    
+}
