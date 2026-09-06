35bacd20460a6563ec680474af3b825e58c1fbba
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-07 00:15:46 +0800

    Create day06.java
    Ya

---
+++ day06.java
@@ -0,0 +1,10 @@
+public class day06 {
+    
+    public static void main(String[] args) {
+        char nilai='A';
+        boolean perempuan=true;
+        System.out.println("Nilai: "+nilai);
+        System.out.println("Aku perempuan: "+perempuan);
+    }
+    
+}
