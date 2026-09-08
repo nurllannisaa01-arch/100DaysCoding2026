50e0f06c6bf5cdfba157019a8e989e6a3d19e1e0
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-09 05:42:03 +0800

    Create day08.java
    Ya

---
+++ day08.java
@@ -0,0 +1,15 @@
+public class day08 {
+    
+    public static void main(String[] args) {
+        //Deklarasi Variabel
+        int umur;
+        //inisialisasi variabel
+        umur = 17;
+        System.out.println("umur awal: "+umur);
+        //update variabel
+        umur = 18;
+        System.out.println("umur setelah di-update: "+umur);
+        
+    }
+    
+}
