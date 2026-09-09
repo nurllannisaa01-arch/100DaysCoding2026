4f785bff7113d2bc61b74dbeef6026e0b5b2eef6
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-10 05:30:46 +0800

    Create day09.java
    .

---
+++ day09.java
@@ -0,0 +1,12 @@
+public class day09 {
+    
+    public static void main(String[] args) {
+        final int harga_baju=17000;
+        int jumlah=2;
+        int total=harga_baju * jumlah;
+        System.out.println("harga baju: Rp"+harga_baju);
+        System.out.println("jumlah buku: "+jumlah);
+        System.out.println("total harga: Rp"+total);
+    }
+    
+}
