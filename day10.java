a899a87818cd83f4ce04c1e29c3c639ac7e03d77
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-11 06:05:07 +0800

    Create day10.java
    .

---
+++ day10.java
@@ -0,0 +1,32 @@
+public class day10 {
+    
+    public static void main(String[] args) {
+        //tipe data byte
+        System.out.println("Byte:");
+        System.out.println("Nilai minimum= "+Byte.MIN_VALUE);
+        System.out.println("Nilai maksimum= "+Byte.MAX_VALUE);
+        //tipe data short
+        System.out.println("\nShort:");
+        System.out.println("Nilai minimum="+Short.MIN_VALUE);
+        System.out.println("Nilai maksimum= "+Short.MAX_VALUE);
+        //tipe data int
+        System.out.println("\nInteger:");
+        System.out.println("Nilai minimum="+Integer.MIN_VALUE);
+        System.out.println("Nilai maksimum= "+Integer.MAX_VALUE);
+        //tipe data long
+        System.out.println("\nLong:");
+        System.out.println("Nilai minimum="+Long.MIN_VALUE);
+        System.out.println("Nilai maksimum="+Long.MAX_VALUE);
+        //tipe data float
+        System.out.println("\nFloat");
+        System.out.println("Nilai minimum="+-Float.MAX_VALUE);
+        System.out.println("Nilai maksimum="+Float.MAX_VALUE);
+        //tipe data Double
+        System.out.println("\nDouble");
+        System.out.println("Nilai minimum="+-Double.MAX_VALUE);
+        System.out.println("Nilai maksimum="+Double.MAX_VALUE);
+        
+        
+    }
+    
+}
