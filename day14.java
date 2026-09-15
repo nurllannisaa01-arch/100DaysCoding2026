61fb002e5c8979633cf2d5ca29a04523a374250d
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-15 20:53:01 +0800

    Create day14.java
    .

---
+++ day14.java
@@ -0,0 +1,16 @@
+public class day14 {
+    
+    public static void main(String[] args) {
+        int angka1=26;
+        int angka2=13;
+        //penjumlahan 
+        int hasilTambah= angka1 + angka2;
+        //pengurangan
+        int hasilKurang= angka1 - angka2;
+        System.out.println("angka 1= "+ angka1);
+        System.out.println("angka 2= "+angka2);
+        System.out.println("hasil penjumlahan ="+ hasilTambah);
+        System.out.println("hasil pengurangan= "+ hasilKurang);
+    }
+    
+}
