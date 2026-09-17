26eed08c8eba0c6bb4683466198c4c1bc0773fa1
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-17 20:42:05 +0800

    Create day16.java
    .

---
+++ day16.java
@@ -0,0 +1,10 @@
+public class day16 {
+    
+    public static void main(String[] args) {
+        int a=10;
+        int b=3;
+        int hasil=a % b;
+        System.out.println(" hasil 10 % 3 = "+hasil);
+    }
+    
+}
