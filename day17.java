ddfcc36e27c682de8384da0f7448f102375c3233
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-18 20:42:00 +0800

    Create day17.java
    .

---
+++ day17.java
@@ -0,0 +1,18 @@
+public class day17 {
+    
+    public static void main(String[] args) {
+        int angka=10;
+        System.out.println("nilai awal: "+angka);
+        angka += 5;
+        System.out.println("setelah angka += 5: "+angka);
+        angka -= 3;
+        System.out.println("setelah angka -= 3: "+angka);
+        angka *= 3;
+        System.out.println("setelah angka *= 3:  "+angka);
+        angka /= 4;
+        System.out.println("setelah angka /= 4: "+angka);
+        angka %= 2;
+        System.out.println("setelah angka %= 2: "+angka);
+    }
+    
+}
