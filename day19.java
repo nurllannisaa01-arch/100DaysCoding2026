ec7b91e7fcd154cc5bad8b0c267b522ec814f26a
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-20 22:20:59 +0800

    Create day19.java
    .

---
+++ day19.java
@@ -0,0 +1,10 @@
+public class day19 {
+    
+    public static void main(String[] args) {
+        double angka=100.1;
+        int hasil = (int)angka;
+        System.out.println("nilai double :"+angka);
+        System.out.println("nilai int :"+hasil);
+    }
+    
+}
