ce860a971297449d3876d6733789b0470079ad36
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-22 22:40:18 +0800

    Update day21.java
    ..

--- day21.java
+++ day21.java
@@ -1,12 +1,28 @@
 public class day21 {
     
     public static void main(String[] args) {
-        String angka="10";
-        int hasil=Integer.parseInt
-        (angka);
-        System.out.println(hasil);
+        String a="10";
+        String b="10.5";
+        String c="true";
+        String d="A";
         
+        byte byteData=Byte.parseByte(a);
+        short shortData=Short.parseShort(a);
+        int intData=Integer.parseInt(a);
+        long longData=Long.parseLong(a);
+        float floatData=Float.parseFloat(b);
+        double doubleData=Double.parseDouble(b);
+         boolean booleanData=Boolean.parseBoolean(c);
+        char charData=d.charAt(0);
         
+        System.out.println(byteData);
+        System.out.println(shortData);
+        System.out.println(intData);
+        System.out.println(longData);
+        System.out.println(floatData);
+        System.out.println(doubleData);
+        System.out.println(booleanData);
+        System.out.println(charData);
     }
     
 }
