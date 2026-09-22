495616017e59575d27c2d8fa0d4a3d286be889a3
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-22 21:33:08 +0800

    Create day21.java
    .

---
+++ day21.java
@@ -0,0 +1,12 @@
+public class day21 {
+    
+    public static void main(String[] args) {
+        String angka="10";
+        int hasil=Integer.parseInt
+        (angka);
+        System.out.println(hasil);
+        
+        
+    }
+    
+}
