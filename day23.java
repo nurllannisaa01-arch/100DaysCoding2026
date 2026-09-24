d33d707fc6807a980a4cc5f7f086e482d98a4488
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-24 17:14:55 +0800

    Create day23.java
    .

---
+++ day23.java
@@ -0,0 +1,10 @@
+public class day23 {
+    
+    public static void main(String[] args) {
+        int sisi=5;
+        int luas=sisi * sisi;
+        System.out.println("sisi persegi :"+sisi);
+        System.out.println("luas persegi : "+luas);
+    }
+    
+}
