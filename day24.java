2bc2c8456e35b0b44895974a42e4b66d1eb4549e
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2026-09-25 20:12:05 +0800

    Create day24.java
    .

---
+++ day24.java
@@ -0,0 +1,12 @@
+public class day24 {
+    
+    public static void main(String[] args) {
+        int panjang=10;
+        int lebar=5;
+        int luas= panjang * lebar;
+        System.out.println("panjang= "+panjang);
+        System.out.println("lebar= "+lebar);
+        System.out.println("luas persegi panjang= "+ luas);
+    }
+    
+}
